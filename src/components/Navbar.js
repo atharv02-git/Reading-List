63ceaa84913394c59945b8f6a67fc1e7ed552f4c
Atharv <atharvbhandare2001@gmail.com>
2022-01-10 23:57:24 +0530

    final touches to navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <div className="navbar">
       <h1>My Reading list: </h1>
-      <p>Currently you have {books.length} books available to read</p>
+      {books.length === 1 ? <p>Currently you have {books.length} book available to read...</p> : <p>Currently you have {books.length} books available to read...</p>}
     </div>
   );
 }
